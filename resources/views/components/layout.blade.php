3e256ef026799b9d858d668111413e64d3f08540
Jomar <jomarsoliman01@gmail.com>
2025-07-09 21:32:30 +0800

    Add comment for important lnk

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -7,8 +7,14 @@
 
     <!-- Tailwind CSS CDN -->
     <script src="https://cdn.tailwindcss.com"></script>
+
+    <!--  FONTAWESOME CDN -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+
+    <!-- DEVICON CDN -->
     <link rel="stylesheet" type='text/css' href="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/devicon.min.css" />
+
+    <!-- CUSTOMIZE CSS -->
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
 </head>
