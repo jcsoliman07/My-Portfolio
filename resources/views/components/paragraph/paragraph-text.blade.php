c5ce9779849a9ad04d716743d11a477cfc52f1f7
Jomar <jomarsoliman01@gmail.com>
2025-07-09 21:14:35 +0800

    Change the carvariantiant to variant

--- resources/views/components/paragraph/paragraph-text.blade.php
+++ resources/views/components/paragraph/paragraph-text.blade.php
@@ -2,7 +2,7 @@
 @props(['variant' => 'default'])
 
 @php
-    $styles = match ($carvariantiant) {
+    $styles = match ($variant) {
         'default' => 'text-gray-600 mb-8 max-w-lg',
         'centered' => 'max-w-2xl mx-auto text-gray-600',
     }
