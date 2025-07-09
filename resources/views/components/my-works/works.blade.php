d1c8518af720f7ea99621aab207717b50ce86515
Jomar <jomarsoliman01@gmail.com>
2025-07-09 21:54:37 +0800

    Add a Heaing for My works components

--- resources/views/components/my-works/works.blade.php
+++ resources/views/components/my-works/works.blade.php
@@ -1,3 +1,7 @@
 <section id="work" class="py-20 px-4 sm:px-6 lg:px-8 bg-gray-50">
-
+    <div class="max-w-7xl mx-auto">
+        <div class="text-center mb-16">
+            <x-heading.heading-2 variant="gradient">My Workds</x-heading.heading-2>
+        </div>
+    </div>
 </section>
