78fb5ff5a1b004eff00bc31f8206de9053329dda
Jomar <jomarsoliman01@gmail.com>
2025-07-09 21:10:44 +0800

    Change the spelling tetx to text

--- resources/views/components/my-skills/skills.blade.php
+++ resources/views/components/my-skills/skills.blade.php
@@ -1,6 +1,6 @@
 <section id="skills" class="py-20 px-4 sm:px-6 lg:px-8 bg-white">
     <div class="max-w-7xl mx-auto">
-        <div class="tetx-center mb-16">
+        <div class="text-center mb-16">
             <x-heading.heading-2 variant="gradient">Tech Stacks</x-heading.heading-2>
         </div>
     </div>
