ce9386fef9c3a4f1731fe3d800f3dfa3b9b94518
Jomar <jomarsoliman01@gmail.com>
2025-07-09 21:16:08 +0800

    Add a div for tech stack card

--- resources/views/components/my-skills/skills.blade.php
+++ resources/views/components/my-skills/skills.blade.php
@@ -7,7 +7,11 @@
             <x-paragraph.paragraph-text variant="centered">
                 Here are the technologies I'm proficient in and use regularly in my projects.
             </x-paragraph.paragraph-text>
-            
+
+        </div>
+
+        <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-7 gap-6">
+
         </div>
     </div>
 </section>
