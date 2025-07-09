eb53735fbfbd33c5e54dbd882ea01da207ad87ae
Jomar <jomarsoliman01@gmail.com>
2025-07-09 20:20:04 +0800

    Create and Add mobile navigation view

---
+++ resources/views/components/navigation/mobile-top-navigation-link.blade.php
@@ -0,0 +1,4 @@
+
+
+<a href="#home" class="block px-3 py-2 rounded-md text-base font-medium text-white hover:bg-gray-800">{{ $slot }}</a>
+
---
+++ resources/views/components/navigation/mobile-top-navigation.blade.php
@@ -0,0 +1,9 @@
+
+<div id="mobile-menu" class="hidden md:hidden bg-white shadow-lg">
+    <div class="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+        <x-navigation.mobile-top-navigation-link>Home</x-navigation.mobile-top-navigation-link>
+        <x-navigation.mobile-top-navigation-link>Skills</x-navigation.mobile-top-navigation-link>
+        <x-navigation.mobile-top-navigation-link>Work</x-navigation.mobile-top-navigation-link>
+        <x-navigation.mobile-top-navigation-link>Contact</x-navigation.mobile-top-navigation-link>
+    </div>
+</div>
