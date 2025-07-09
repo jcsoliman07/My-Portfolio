2817cb76eba3d8507c50c4323f338e51f79fdfd4
Jomar <jomarsoliman01@gmail.com>
2025-07-09 20:22:48 +0800

    Create a global js with Mobile Menu Toggle function

---
+++ public/js/global.js
@@ -0,0 +1,4 @@
+// Mobile menu toggle
+document.getElementById('mobile-menu-button').addEventListener('click', function() {
+    document.getElementById('mobile-menu').classList.toggle('hidden');
+});
