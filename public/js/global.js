5ce9c14a19c5154c792edbb70a462dc31053893b
Jomar <jomarsoliman01@gmail.com>
2025-07-09 22:21:26 +0800

    Add a Smooth Scrolling Anchor Links

--- public/js/global.js
+++ public/js/global.js
@@ -2,3 +2,17 @@
 document.getElementById('mobile-menu-button').addEventListener('click', function() {
     document.getElementById('mobile-menu').classList.toggle('hidden');
 });
+
+
+// Smooth scrolling for anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        
+        document.getElementById('mobile-menu').classList.add('hidden');
+        
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
